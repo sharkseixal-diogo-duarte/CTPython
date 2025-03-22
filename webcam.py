48eab3f1accea9695ef40c6afb8fbb6f9297add9
sharkseixal-diogo-duarte <shark04050@sharkcoders.pt>
2025-03-22 14:56:25 +0000

    webcam.py

--- webcam.py
+++ webcam.py
@@ -1,21 +1,32 @@
-import cv2
-import numpy
 import numpy as np
+import cv2
 
-capt = cv2.VideoCapture(0)
-
+webcam = cv2.VideoCapture(0)
 
 while True:
-    ret, frame = capt.read()
-    width = int(capt.get(3))
-    height = int(capt.get(4))
-    image = np.zeros(frame.shapes, np.uint8)
-    small_frame = cv2.resize(frame / 0.5)
-    rotate = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
-    cv2.imshow('webcam', small_frame)
+    ret, frame = webcam.read()
+    width = int(webcam.get(3))
+    height = int(webcam.get(4))
+
+    # Criar uma imagem do tamanho da frame da webcam e por todos os pixeis a "zero"
+    image = np.zeros(frame.shape, np.uint8)
+
+    # Resize the frame a metade no eixo de x e y
+    small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+
+    # Preencher a image com a frame redimensionada
+    image[:height // 2, :width // 2] = small_frame
+    image[height // 2:, :width // 2] = small_frame
+    image[:height // 2, width // 2:] = small_frame
+    image[height // 2:, width // 2:] = small_frame
+
+    # Preencher a image com a frame redimensionada c/ rotações das small_frame's
+    ...
+
+    cv2.imshow('frame', image)
+
     if cv2.waitKey(1) == ord('q'):
         break
 
-
-capt.release()
+webcam.release()
 cv2.destroyAllWindows()
