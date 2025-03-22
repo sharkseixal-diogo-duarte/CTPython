45fd95131cfb64e0fefc3716073ebd84b37db0ea
sharkseixal-diogo-duarte <shark04050@sharkcoders.pt>
2025-03-22 17:51:34 +0000

    desenhar na webcam.py

--- desenhar na webcam.py
+++ desenhar na webcam.py
@@ -12,13 +12,15 @@ while True:
     image = np.zeros(frame.shape, np.uint8)
 
     # Resize the frame a metade no eixo de x e y
-    small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
-
+    small_frame_1 = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+    small_frame_2 = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+    small_frame_3 = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+    small_frame_4 = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
     # Preencher a image com a frame redimensionada
-    image[:height // 2, :width // 2] = small_frame
-    image[height // 2:, :width // 2] = small_frame
-    image[:height // 2, width // 2:] = small_frame
-    image[height // 2:, width // 2:] = small_frame
+    image[:height // 2, :width // 2] = small_frame_1
+    image[height // 2:, :width // 2] = small_frame_2
+    image[:height // 2, width // 2:] = small_frame_3
+    image[height // 2:, width // 2:] = small_frame_4
 
     # Preencher a image com a frame redimensionada c/ rotações das small_frame's
     #image[:height // 2, :width // 2] = small_frame # small frame "normal"
@@ -27,15 +29,16 @@ while True:
     #image[:height // 2, width // 2:] = cv2.rotate(stretched_frame, cv2.ROTATE_90_CLOCKWISE)
     #image[height // 2:, width // 2:] = cv2.rotate(small_frame, cv2.ROTATE_180)
 
-    #img = cv2.line(frame, (0,0), (width, height), (255,0,0), 10)
-    #img = cv2.line(img, (0, height), (width, 0), (0, 255, 0), 5)
-    #img = cv2.rectangle(img, (100, 100), (200, 200), (128, 128, 128), 5)
-    #img = cv2.circle(img,(320, 240), 60, (0, 0, 255), -1)
+    img = cv2.line(frame, (0,0), (width, height), (255,0,0), 10)
+    img = cv2.line(img, (0, height), (width, 0), (0, 255, 0), 5)
+    img = cv2.rectangle(img, (100, 100), (200, 200), (128, 128, 128), 5)
+    img = cv2.circle(img,(320, 240), 60, (0, 0, 255), -1)
 
-    image[:height // 2, :width // 2] = cv2.rectangle(small_frame, (0, 0), (320, 240), (0, 0, 255), 5)
-    image[height // 2:, :width // 2] = small_frame
-    image[:height // 2, width // 2:] = cv2.circle(small_frame,(160, 120), 60, (255, 0, 0), -1)
-    image[height // 2:, width // 2:] = small_frame
+
+    image[:height // 2, :width // 2] = cv2.rectangle(small_frame_1,(0, 0),(320, 240),(0, 0, 255),5)
+    image[height // 2:, :width // 2] = cv2.circle(small_frame_2,(160, 120),45,(255, 0, 0),-1)
+    image[:height // 2, width // 2:] = cv2.line(small_frame_3, (0,0), (320,240), (0, 255, 0), 5)
+    image[height // 2:, width // 2:] = cv2.rectangle(small_frame_4,(100, 100),(200, 200),(0, 255, 0),-1)
 
 
     cv2.imshow('desenho na webcam', image)
