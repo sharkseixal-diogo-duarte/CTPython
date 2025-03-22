c39aec6858f46f65f94378a30b62cc01e321073e
sharkseixal-diogo-duarte <shark04050@sharkcoders.pt>
2025-03-22 16:04:50 +0000

    Update desenhar na webcam.py

--- desenhar na webcam.py
+++ desenhar na webcam.py
@@ -21,18 +21,24 @@ while True:
     image[height // 2:, width // 2:] = small_frame
 
     # Preencher a image com a frame redimensionada c/ rotações das small_frame's
-    image[:height // 2, :width // 2] = small_frame # small frame "normal"
-    stretched_frame = cv2.resize(small_frame, (240, 320))
-    image[height // 2:, :width // 2] = cv2.rotate(stretched_frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
-    image[:height // 2, width // 2:] = cv2.rotate(stretched_frame, cv2.ROTATE_90_CLOCKWISE)
-    image[height // 2:, width // 2:] = cv2.rotate(small_frame, cv2.ROTATE_180)
+    #image[:height // 2, :width // 2] = small_frame # small frame "normal"
+    #stretched_frame = cv2.resize(small_frame, (240, 320))
+    #image[height // 2:, :width // 2] = cv2.rotate(stretched_frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    #image[:height // 2, width // 2:] = cv2.rotate(stretched_frame, cv2.ROTATE_90_CLOCKWISE)
+    #image[height // 2:, width // 2:] = cv2.rotate(small_frame, cv2.ROTATE_180)
 
-    img = cv2.line(small_frame, (0,0), (width, height), (255,0,0), 10)
-    img = cv2.line(img, (0, height), (width, 0), (0, 255, 0), 5)
-    img = cv2.rectangle(img, (100, 100), (200, 200), (128, 128, 128), 5)
-    img = cv2.circle(img,(320, 240), 60, (0, 0, 255), -1)
+    #img = cv2.line(frame, (0,0), (width, height), (255,0,0), 10)
+    #img = cv2.line(img, (0, height), (width, 0), (0, 255, 0), 5)
+    #img = cv2.rectangle(img, (100, 100), (200, 200), (128, 128, 128), 5)
+    #img = cv2.circle(img,(320, 240), 60, (0, 0, 255), -1)
 
-    cv2.imshow('4 webcam', image)
+    image[:height // 2, :width // 2] = cv2.rectangle(small_frame, (0, 0), (320, 240), (0, 0, 255), 5)
+    image[height // 2:, :width // 2] = small_frame
+    image[:height // 2, width // 2:] = cv2.circle(small_frame,(160, 120), 60, (255, 0, 0), -1)
+    image[height // 2:, width // 2:] = small_frame
+
+
+    cv2.imshow('desenho na webcam', image)
 
     if cv2.waitKey(1) == ord('q'):
         break
