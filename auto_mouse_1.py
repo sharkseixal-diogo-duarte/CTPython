55756325f918e3392042019078f44772b84c8aaf
sharkseixal-diogo-duarte <shark04050@sharkcoders.pt>
2025-06-21 10:54:50 +0100

    projeto melhorado

--- auto_mouse_1.py
+++ auto_mouse_1.py
@@ -1,5 +1,32 @@
+import cv2
+import numpy as np
 import pyautogui
+import keyboard
+test_area = (400, 200, 220, 150)
 
-while True:
-    mouse_position = pyautogui.position()
-    print(mouse_position)
+try:
+    while True:
+        if keyboard.is_pressed('q'):
+            break
+
+        screenshot = pyautogui.screenshot(region=test_area)
+        gray = cv2.cvtColor(np.array(screenshot), cv2.COLOR_BGR2GRAY)
+        _, thresh = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+
+        obstacle_area = thresh[60:100, 150:180]
+        obstacle_pixels = cv2.countNonZero(obstacle_area)
+
+        print("Pixels detectados:", obstacle_pixels)
+
+        if (50 < obstacle_pixels < 999) or (1000 < obstacle_pixels < 1190):
+            pyautogui.press("space")
+
+        cv2.imshow("Área de Detecção", thresh)
+
+        if cv2.waitKey(100) & 0xFF == ord('q'):
+            break
+
+except KeyboardInterrupt:
+    print("Interrompido pelo usuário.")
+finally:
+    cv2.destroyAllWindows()
