69662a08659cba7b37a00afc5e66402cc1230acf
sharkseixal-diogo-duarte <shark04050@sharkcoders.pt>
2025-11-21 17:35:03 +0000

    Update urls.py

--- blogproject/blogapp/urls.py
+++ blogproject/blogapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', PostListView.as_view(),name='post-list'),
     path('post/<int:pk>/', PostDetailView.as_view(),name='post_detail'),
     path('post/new/', PostCreateView.as_view(), name='post_create'),
+
 ]
