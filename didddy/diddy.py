486cb8f719bfced3e0144cc49b1a3d390fb58644
sharkseixal-diogo-duarte <shark04050@sharkcoders.pt>
2026-02-06 18:28:09 +0000

    Add github function to open GitHub in Chrome

--- didddy/diddy.py
+++ didddy/diddy.py
@@ -1,13 +1,24 @@
 import pyautogui
 from time import sleep
+
 def youtube():
     pyautogui.press("win")
-    sleep(1)
+    sleep(0.6)
     pyautogui.write("chrome")
-    sleep(1)
+    sleep(0.6)
     pyautogui.press("enter")
-    sleep(1)
+    sleep(0.6)
     pyautogui.write("youtube.com")
-    sleep(1)
+    sleep(0.6)
     pyautogui.press("enter")
-youtube()
+
+def github():
+    pyautogui.press("win")
+    sleep(0.6)
+    pyautogui.write("chrome")
+    sleep(0.6)
+    pyautogui.press("enter")
+    sleep(0.6)
+    pyautogui.write("github.com")
+    sleep(0.6)
+    pyautogui.press("enter")
